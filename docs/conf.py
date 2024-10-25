beec85cd48cbbbaaac79df2bceb8349b4357b068
Christos Plachouras <cplachouras@gmail.com>
2024-10-25 10:22:05 +0100

    update release version

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "synesis"
 copyright = "2024, Christos, Julien"
 author = "Christos, Julien"
-release = "0.0"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
