de505c8c0796559bcf0e776ddf75591be6e5752b
chrispla <cplachouras@gmail.com>
2024-12-29 21:41:02 +0000

    ijepa vit h 14

---
+++ synesis/features/ijepa.py
@@ -0,0 +1,22 @@
+"""I-JEPA feature extractor model."""
+
+import torch
+from torch import nn
+from transformers import AutoModel
+
+
+class IJEPA(nn.Module):
+    def __init__(self, feature_extractor=True):
+        super(IJEPA, self).__init__()
+
+        self.feature_extractor = feature_extractor
+
+        self.encoder = AutoModel.from_pretrained("jmtzt/ijepa_vith14_1k")
+
+    def forward(self, x):
+        if self.feature_extractor:
+            with torch.no_grad():
+                h = self.encoder(pixel_values=x).last_hidden_state.mean(dim=1)
+                return h
+        else:
+            raise NotImplementedError("Training not implemented yet.")
