3ebd8bf72238e6ad015d68a706763a0910c269b1
chrispla <cplachouras@gmail.com>
2024-12-29 15:20:45 +0000

    vit b 16 supervised imagenet 1k

---
+++ synesis/features/vit_imagenet.py
@@ -0,0 +1,32 @@
+"""ViT model for feature extraction."""
+
+import torch
+from torch import nn
+from torchvision.models import vit_b_16  # or another ViT model varian
+
+
+class ViT_ImageNet(nn.Module):
+    def __init__(self, feature_extractor=True):
+        super(ViT_ImageNet, self).__init__()
+
+        self.feature_extractor = feature_extractor
+
+        # Load the pretrained ViT model
+        self.encoder = vit_b_16(weights="IMAGENET1K_V1")
+        self.encoder.heads = (
+            nn.Identity()
+        )  # Replace the classification head with an identity layer
+
+    def forward(self, x):
+        if self.feature_extractor:
+            with torch.no_grad():
+                h = self.encoder(x)
+                return h
+        else:
+            raise NotImplementedError("Training not implemented yet.")
+
+
+# Example usage
+if __name__ == "__main__":
+    model = ViT_ImageNet()
+    print(model)
