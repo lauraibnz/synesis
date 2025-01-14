2bc0621f43bb1b60d605a1663b37d59aa4aac8df
chrispla <cplachouras@gmail.com>
2025-01-14 16:26:11 +0000

    add all used configs

--- config/equivariance/parameters.py
+++ config/equivariance/parameters.py
@@ -21,4 +21,99 @@ configs = {
             "batch_size": 32,
         },
     },
+    "long": {
+        "model": {
+            "type": "regressor",
+            "params": {"hidden_units": [512, 256]},
+        },
+        "training": {
+            "criterion": nn.MSELoss,
+            "optimizer": {"class": Adam, "params": {"lr": 0.001, "weight_decay": 0.01}},
+            "batch_size": 32,
+            "num_epochs": 10,
+            "patience": 5,
+            "feature_aggregation": False,
+        },
+        "evaluation": {
+            "criterion": nn.MSELoss,
+            "feature_aggregation": False,
+            "batch_size": 32,
+        },
+    },
+    "regression": {
+        "model": {
+            "type": "regressor",
+            "params": {"hidden_units": [512, 256]},
+        },
+        "training": {
+            "criterion": nn.MSELoss,
+            "optimizer": {"class": Adam, "params": {"lr": 0.001, "weight_decay": 0.01}},
+            "batch_size": 32,
+            "num_epochs": 5,
+            "patience": 3,
+            "feature_aggregation": False,
+        },
+        "evaluation": {
+            "criterion": nn.MSELoss,
+            "feature_aggregation": False,
+            "batch_size": 32,
+        },
+    },
+    "regression_linear": {
+        "model": {
+            "type": "regressor",
+            "params": {"hidden_units": []},
+        },
+        "training": {
+            "criterion": nn.MSELoss,
+            "optimizer": {"class": Adam, "params": {"lr": 0.001, "weight_decay": 0.01}},
+            "batch_size": 32,
+            "num_epochs": 5,
+            "patience": 3,
+            "feature_aggregation": False,
+        },
+        "evaluation": {
+            "criterion": nn.MSELoss,
+            "feature_aggregation": False,
+            "batch_size": 32,
+        },
+    },
+    "linear": {
+        "model": {
+            "type": "regressor",
+            "params": {"hidden_units": []},
+        },
+        "training": {
+            "criterion": nn.MSELoss,
+            "optimizer": {"class": Adam, "params": {"lr": 0.001, "weight_decay": 0.01}},
+            "batch_size": 32,
+            "num_epochs": 5,
+            "patience": 3,
+            "feature_aggregation": False,
+        },
+        "evaluation": {
+            "criterion": nn.MSELoss,
+            "feature_aggregation": False,
+            "batch_size": 32,
+        },
+    },
+    "long_linear": {
+        "model": {
+            "type": "regressor",
+            "params": {"hidden_units": []},
+        },
+        "training": {
+            "criterion": nn.MSELoss,
+            "optimizer": {"class": Adam, "params": {"lr": 0.001, "weight_decay": 0.01}},
+            "batch_size": 32,
+            "num_epochs": 10,
+            "patience": 5,
+            "feature_aggregation": False,
+        },
+        "evaluation": {
+            "criterion": nn.MSELoss,
+            "feature_aggregation": False,
+            "batch_size": 32,
+        },
+    },
 }
