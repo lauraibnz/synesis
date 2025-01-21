282a9725e78ac38d108e9733bddc2d1b0e23c2e7
chrispla <cplachouras@gmail.com>
2025-01-21 15:06:01 +0000

    add reverb, controllable by rt60

--- config/transforms.py
+++ config/transforms.py
@@ -1,4 +1,4 @@
-from audiomentations import TimeStretch
+from audiomentations import RoomSimulator, TimeStretch
 from torch_audiomentations import AddColoredNoise, PitchShift
 
 configs = {
@@ -28,6 +28,36 @@ configs = {
             "min_rate": 0.5,
             "max_rate": 2,
             "leave_length_unchanged": False,
+            "p": 1,
+        },
+    },
+    "AddReverb": {
+        "class": RoomSimulator,
+        "params": {
+            "min_target_rt60": 0.0,
+            "max_target_rt60": 3.0,
+            "min_size_x": 5.6,
+            "max_size_x": 5.6,
+            "min_size_y": 3.9,
+            "max_size_y": 3.9,
+            "min_size_z": 3.0,
+            "max_size_z": 3.0,
+            "min_source_x": 3.5,
+            "max_source_x": 3.5,
+            "min_source_y": 2.7,
+            "max_source_y": 2.7,
+            "min_source_z": 2.1,
+            "max_source_z": 2.1,
+            "min_mic_distance": 0.35,
+            "max_mic_distance": 0.35,
+            "min_mic_azimuth": 0,
+            "max_mic_azimuth": 0,
+            "min_mic_elevation": 0,
+            "max_mic_elevation": 0,
+            "use_ray_tracing": True,
+            "calculation_mode": "rt60",
+            "max_order": 4,
+            "leave_length_unchanged": True,
             "p": 1,
         },
     },
