abb40d378a35e18fe3aafdd31626223e74956530
chrispla <cplachouras@gmail.com>
2025-01-21 14:17:28 +0000

    add reverb transform, controllable by rt60

--- synesis/transforms/transform_utils.py
+++ synesis/transforms/transform_utils.py
@@ -7,7 +7,7 @@ def get_transform(transform_config, **kwargs):
     transform_params = transform_config["params"]
     if kwargs:
         transform_params.update(kwargs)
-    if transform_class.__name__ != "TimeStretch":
+    if transform_class.__name__ not in ["TimeStretch", "RoomSimulator"]:
         transform_params["output_type"] = "tensor"
     transform = transform_class(**transform_params)
     return transform
