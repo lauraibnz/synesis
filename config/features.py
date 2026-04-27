69114e56227d715c877fb13f7fd24a23a3bdd72d
lauraibnz <laura.ibanez@upf.edu>
2026-04-27 20:26:04 +0200

    Add mld no pitch

--- config/features.py
+++ config/features.py
@@ -313,6 +313,26 @@ configs = {
             "use_ema": True,
         }
     },
+    "MLD_base_no_pitch_Timbre": {
+        "__cls__": "MLD_Timbre",
+        "sample_rate": 44100,
+        "extract_kws": {
+            "repo_path": "./externals/music-latent-disentanglement",
+            "checkpoint_path": "experiments/runs/base_no_pitch/best_model.pt",
+            "config_path": "experiments/runs/base_no_pitch/config.gin",
+            "use_ema": True,
+        }
+    },
+    "MLD_base_no_pitch_Structure": {
+        "__cls__": "MLD_Structure",
+        "sample_rate": 44100,
+        "extract_kws": {
+            "repo_path": "./externals/music-latent-disentanglement",
+            "checkpoint_path": "experiments/runs/base_no_pitch/best_model.pt",
+            "config_path": "experiments/runs/base_no_pitch/config.gin",
+            "use_ema": True,
+        }
+    },
     "AFTER_Timbre": {
         "__cls__": "AFTER_Timbre",
         "sample_rate": 44100,
